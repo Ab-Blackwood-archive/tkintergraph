176290586bfe7bc8fc254fc5ac48b4789b601937
Ab-Blackwood-archive <ab061906@wgcloud.org>
2024-01-24 11:35:09 -0600

    added code

---
+++ Tkinterdraw.py
@@ -0,0 +1,83 @@
+
+from ctypes.wintypes import HBITMAP
+import tkinter as tk
+from turtle import ht
+
+
+#custom functions
+
+
+def draw():
+    x1 = int(txtX1.get())
+    y1 = int(txtY1.get())
+    x2 = int(txtX2.get())
+    y2 = int(txtY2.get())
+    canvas.create_line(x1,y1,x2,y2)
+def clear():
+    canvas.delete('all')
+
+def graphline():
+    xhs =  10         # x of horz starting point same
+    yhs =  10         # y of horz starting point 
+    xhe = 435         # x of horz end point same
+    yhe = 10  
+    xvs = 10          # x of vert starting  point 
+    yvs = 10          # y of vert starting point same
+    xve = 10          # x of vert end point
+    yve = 435
+    for i in range(18): # draws horizontal grid lines
+        canvas.create_line(xhs, yhs, xhe, yhe)
+        yhs = yhs + 25
+        yhe = yhe + 25
+    for i in range(18): # draws vert grid lines
+        canvas.create_line(xvs, yvs, xve, yve)
+        xvs = xvs + 25
+        xve = xve + 25
+    canvas.create_line(212, 5, 212, 440)
+    canvas.create_line(5, 212, 440, 212)
+# canvas dimentions
+canvas_width = 450
+canvas_height = 450
+
+#window properties
+window = tk.Tk()
+window.title("graphing with Tkinter")
+window.geometry("800x800") 
+
+#create lables
+lblev = tk.Label( window, text = "X1: ")
+lblrv = tk.Label( window, text = "Y1: ")
+lblX2 = tk.Label( window, text = "X2: ")
+lblY2 = tk.Label( window, text = "Y2: ")
+
+
+#read textboxs
+txtX1 = tk.Entry(window)
+txtY1 = tk.Entry(window)
+txtX2 = tk.Entry(window)
+txtY2 = tk.Entry(window)
+
+#buttons
+btn = tk.Button(window, text="Draw!", command= draw)
+btnClear = tk.Button(window, text = "Clear", padx= 20, command=clear)
+#canvas
+canvas = tk.Canvas(window,  width=canvas_width, height=canvas_height, bg="white")
+
+
+#add GUI items to grid 
+lblev.grid(row=0, column = 0)
+lblrv.grid(row= 1, column = 0)
+lblX2.grid(row =2, column = 0)
+lblY2.grid(row= 3, column = 0)
+txtX1.grid(row= 0, column = 1)
+txtY1.grid(row= 1, column = 1)
+txtX2.grid(row =2, column = 1)
+txtY2.grid(row= 3, column = 1)
+btn.grid(row= 3, column= 3)
+btnClear.grid(row= 3, column= 4)
+canvas.grid(row=4, column= 0)
+
+
+#build window
+graphline()
+window.mainloop()
