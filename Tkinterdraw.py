c014d1da87410dc5d278e7dfa25e6073154e8f9e
Ab-Blackwood-archive <ab061906@wgcloud.org>
2024-02-01 11:17:42 -0600

    finished grid, dases, line

--- Tkinterdraw.py
+++ Tkinterdraw.py
@@ -8,7 +8,8 @@ from turtle import ht
 # shift center point rduce size o
 # add slope calculationm 
 # difine left and bottom as -
-# if x-(num) then put on left y-(num) put on bottom otherwise put r or t 10 to 440 = 420 210 each side
+# if x =-(num) then put on left y =-(num) put on bottom otherwise put r or t 10 to 440 = 420 210 each side
+# break up grid to numbers 100 up by ten 500px wide each sopot 25 px
 # dots for marks markes for numbers 
 
 #y = mx + b                                                                                                                                                           
@@ -20,34 +21,47 @@ def draw():
     y2 = int(txtY2.get())
     canvas.create_line(x1,y1,x2,y2)
 def clear():
-    canvas.delete('all')
+    canvas.delete('draw')
 
 def graphline():
     xhs =  20         # x of horz starting point same
     yhs =  20         # y of horz starting point 
-    xhe = 440         # x of horz end point same
-    yhe = 10  
-    xvs = 10          # x of vert starting  point 
-    yvs = 10          # y of vert starting point same
-    xve = 10          # x of vert end point
-    yve = 460
-    for i in range(19): # draws horizontal grid lines
+    xhe = 520         # x of horz end point same
+    yhe = 20 
+    xvs = 20        # x of vert starting  point 
+    yvs = 20          # y of vert starting point same
+    xve = 20          # x of vert end point
+    yve = 520
+    for i in range(21): # draws horizontal grid lines
         canvas.create_line(xhs, yhs, xhe, yhe)
         yhs = yhs + 25
         yhe = yhe + 25
-    for i in range(19): # draws vert grid lines
+    for i in range(21): # draws vert grid lines
         canvas.create_line(xvs, yvs, xve, yve)
         xvs = xvs + 25
         xve = xve + 25
-    canvas.create_line(210, 10, 210, 470, width =3) # cross demarcation
-    canvas.create_line(10, 2, 460, 210, width=3) # cross demarcation 
-
-
+    canvas.create_line(270, 10, 270, 530, width =3) # cross demarcation hoz
+    canvas.create_line(10, 270, 530, 270, width=3) # cross demarcation vert 
+    hd = 45
+    vd = 45
+    for i in range(19): #draws dashes 
+        canvas.create_line(hd,275,hd,265, width = 2) # hoz dash
+        canvas.create_line(275,vd,265,vd, width = 2) # vert dash
+        hd = hd + 25
+        vd = vd + 25
+"""""
+if (-) :#places negaitive number
+20 + 25
+elif >0:  #places posiotive numbers if 
+270 + 25
+else: # places 0 at middle
+    = 270
+"""
 #def slope
 
 # canvas dimentions666
-canvas_width = 480
-canvas_height = 480
+canvas_width = 540
+canvas_height = 540
 
 #window properties
 window = tk.Tk()
