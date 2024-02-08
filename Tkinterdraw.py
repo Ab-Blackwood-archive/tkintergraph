c82f5a6bb420f9af645dc755c31381f6ddea0301
Ab-Blackwood-archive <ab061906@wgcloud.org>
2024-02-08 11:02:36 -0600

    simplifyed

--- Tkinterdraw.py
+++ Tkinterdraw.py
@@ -6,7 +6,6 @@ from turtle import ht
 
 #custom functions
 # shift center point rduce size o
-# add slope calculationm 
 # difine left and bottom as -
 # if x =-(num) then put on left y =-(num) put on bottom otherwise put r or t 10 to 440 = 420 210 each side figure out the convertion ratio 1 = 2.5 px
 # break up grid to numbers 100 up by ten 500px wide each sopot 25 px 
@@ -25,21 +24,23 @@ def position(point):
 def draw():
     x1 = int(txtX1.get())
     position(x1) # set point to x1
-    point =x1 # convert back to x1
+    point = x1 # convert back to x1
     y1 = int(txtY1.get())
     position(y1)
-    point =y1
+    point = y1
     x2 = int(txtX2.get())
     position(x2)
     point = x2
     y2 = int(txtY2.get())
     position(y2)
     point = y2
-    drwline =canvas.create_line(x1,y1,x2,y2, fill = "#009A44")
+    drwline = canvas.create_line(x1,y1,x2,y2, fill = "#009A44")
 
     return x1
+''''
 def clear():
-    canvas.delete('drwline') #makes it so delete doesnt delete grid spesify lines
+    canvas.delete(drwline) #makes it so delete doesnt delete grid spesify lines
+'
 
 def graphline():
     xhs =  20         # x of horz starting point same
@@ -67,17 +68,21 @@ def graphline():
         canvas.create_line(275,vd,265,vd, width = 2) # vert dash
         hd = hd + 25
         vd = vd + 25
-"""""
-if draw() == "-":#places negaitive number side
-= 270 - 2.5* #number entered
-elif draw() >0:  #places posi tive number
-= 270 + 2.5* # number entered
-else: # places 0 at middle
-    = 270
-"""
-def slope():
-    sx = x2 - x1 
-    sy = y2 - y1 
+'''''
+def graph():
+     hd = 45
+    vd = 45
+    for i in range(19): #draws dashes 
+        canvas.create_line(hd,275,hd,265, width = 2) # hoz dash
+        canvas.create_line(275,vd,265,vd, width = 2) # vert dash
+        hd = hd + 25
+        vd = vd + 25
+    canvas.create_line(40, 500, 40, 50, width=2) # cross demarcation vert 
+    canvas.create_line(40, 500, 530, 500, width=2) # cross demarcation vert 
+    vd = 45
+    for i in range(19): #draws dashes 
+        canvas.create_line(275,vd,265,vd, width = 2) # vert dash  
+        vd = vd + 25
 # canvas dimentions666
 canvas_width = 540
 canvas_height = 540
@@ -102,15 +107,15 @@ txtY2 = tk.Entry(window)
 
 #buttons
 btn = tk.Button(window, text="Draw!", command= draw)
-btnClear = tk.Button(window, text = "Clear", padx= 20, command=clear)
-btnslp = tk.Button(window, text = "Find slope")
+#btnClear = tk.Button(window, text = "Clear", padx= 20, command=clear)
+
 #canvas
 canvas = tk.Canvas(window,  width=canvas_width, height=canvas_height, bg="white")
 
 
 #add GUI items to grid 
 btn.grid(row= 3, column= 3)
-btnClear.grid(row= 3, column= 4)
+#btnClear.grid(row= 3, column= 4)
 canvas.grid(row= 4, column= 0)
 lblev.grid(row= 0, column = 0)
 lblrv.grid(row= 1, column = 0)
@@ -120,9 +125,8 @@ txtX1.grid(row= 0, column = 1)
 txtY1.grid(row= 1, column = 1)
 txtX2.grid(row= 2, column = 1)
 txtY2.grid(row= 3, column = 1)
-btnslp.grid(row= 5, column = 0)
 lblslp.grid(row= 6, column = 0)
 
 #build window
-graphline()
+graph()
 window.mainloop()
